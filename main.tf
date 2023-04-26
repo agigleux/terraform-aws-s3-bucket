e74556eaf064b3a0ce53344497dcf6e5c39f27eb
Alexandre Gigleux <alexandre.gigleux@gmail.com>
2023-04-26 16:50:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,17 @@ locals {
   metric_configuration = try(jsondecode(var.metric_configuration), var.metric_configuration)
 }
 
+resource "aws_s3_bucket2" "this" {
+  count = local.create_bucket ? 1 : 0
+
+  bucket        = var.bucket
+  bucket_prefix = var.bucket_prefix
+
+  force_destroy       = var.force_destroy
+  object_lock_enabled = var.object_lock_enabled
+  tags                = var.tags
+}
+
 resource "aws_s3_bucket" "this" {
   count = local.create_bucket ? 1 : 0
 
